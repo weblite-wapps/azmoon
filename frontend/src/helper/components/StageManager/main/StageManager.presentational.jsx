9d5a402a93f1a10ed07ee76db11f3c37bcc1a4db
AmirHossein Ebrahimi <amirhosseinebrahimi77@gmail.com>
2019-11-07 16:31:37 +0330

    feat(azmoon): Add second verification for ending

--- frontend/src/helper/components/StageManager/main/StageManager.presentational.jsx
+++ frontend/src/helper/components/StageManager/main/StageManager.presentational.jsx
@@ -2,12 +2,38 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 // components
+import Button from '@material-ui/core/Button'
+import Typography from '@material-ui/core/Typography'
+import Slide from '@material-ui/core/Slide'
+import Snackbar from '@material-ui/core/Snackbar'
+import { makeStyles } from '@material-ui/core/styles'
+
 import StageManagerInfo from '../components/StageManagerInfo/StageManagerInfo.presentational'
 import ArrowButton from '../components/ArrowButton/ArrowButton.presentational'
 // helpers
 import { toPersian } from '../../../functions/utils.helper'
 // style
 import './StageManager.scss'
+
+const useStyles = makeStyles(() => ({
+  anchorOriginBottomCenter: {
+    bottom: 'unset',
+    top: 130,
+    '& > div > div:nth-child(2)': {
+      width: '100%',
+      marginLeft: 'unset',
+      padding: 0,
+    },
+  },
+  typography: {
+    fontSize: 12,
+    lineHeight: '17px',
+    letterSpacing: -0.07,
+  },
+  text: {
+    flexGrow: 2,
+  },
+}))
 
 const StageManager = ({
   error,
@@ -20,27 +46,75 @@ const StageManager = ({
   finalStage,
   firstStage,
   examMode,
-}) => (
-  <div className="recast--stage-manager">
-    {examMode && finalStage && (
-      <ArrowButton
-        type="Titled"
-        title={finalStageLabel}
-        onClick={onFinalStageClick}
+}) => {
+  const classes = useStyles()
+  const [open, setOpen] = React.useState(false)
+
+  const onClose = () => {
+    setOpen(false)
+  }
+
+  return (
+    <div className="recast--stage-manager">
+      {examMode && finalStage && (
+        <>
+          <ArrowButton
+            type="Titled"
+            title={finalStageLabel}
+            onClick={() => setOpen(true)}
+          />
+          <Snackbar
+            open={open}
+            className={`${classes.anchorOriginBottomCenter} ${classes.action}`}
+            TransitionComponent={props => <Slide {...props} direction="down" />}
+            ContentProps={{
+              'aria-describedby': 'message-id',
+            }}
+            action={
+              <>
+                <Button
+                  color="inherit"
+                  size="small"
+                  className={classes.typography}
+                  onClick={() => {
+                    onFinalStageClick()
+                    onClose()
+                  }}
+                >
+                  بله
+                </Button>
+
+                <Button
+                  color="inherit"
+                  size="small"
+                  onClick={onClose}
+                  className={classes.typography}
+                >
+                  نه
+                </Button>
+                <Typography
+                  align="right"
+                  id="message-id"
+                  className={`${classes.typography} ${classes.text}`}
+                >
+                  آیا مطمئن هستید؟
+                </Typography>
+              </>
+            }
+          />
+        </>
+      )}
+      {!finalStage && <ArrowButton type="Left" onClick={onLeftClick} />}
+      <StageManagerInfo
+        stageName={toPersian(stageName)}
+        stageLevel={toPersian(stageLevel)}
       />
-    )}
-    {!finalStage && <ArrowButton type="Left" onClick={onLeftClick} />}
-
-    <StageManagerInfo
-      stageName={toPersian(stageName)}
-      stageLevel={toPersian(stageLevel)}
-    />
-
-    {!firstStage && (
-      <ArrowButton type="Right" onClick={onRightClick} hasError={error} />
-    )}
-  </div>
-)
+      {!firstStage && (
+        <ArrowButton type="Right" onClick={onRightClick} hasError={error} />
+      )}
+    </div>
+  )
+}
 
 StageManager.propTypes = {
   error: PropTypes.bool,

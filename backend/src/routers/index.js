943cbe0214325c09a547928355c6a0b4e6723f69
Masoud Salehi <msk.masoud.74@gmail.com>
2019-11-11 20:57:45 +0330

    feat(server): Add /school to get schools in a county

--- backend/src/routers/index.js
+++ backend/src/routers/index.js
@@ -2,10 +2,12 @@ const Router = require('@koa/router')
 const user = require('./user')
 const exam = require('./exam')
 const result = require('./result')
+const school = require('./school')
 
 const router = new Router()
   .use('/api/user', user.routes())
   .use('/api/exam', exam.routes())
   .use('/api/result', result.routes())
+  .use('/api/school', school.routes())
 
 module.exports = router
